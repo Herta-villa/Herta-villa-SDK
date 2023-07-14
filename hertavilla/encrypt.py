9cddd1fb367c08366987c17f80e213085421a156
MingxuanGame <MingxuanGame@outlook.com>
2023-07-14 20:03:17 +0800

    :bug: fix message of rsa.verify is not correct

--- hertavilla/encrypt.py
+++ hertavilla/encrypt.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import base64
-import hashlib
 import urllib.parse
 
 from hertavilla.bot import VillaBot
@@ -17,11 +16,10 @@ def verify(
 ) -> bool:
     sign_ = base64.b64decode(sign)
     sign_msg = urllib.parse.urlencode({"body": body, "secret": bot.secret})
-    hashed_origin = hashlib.sha256(sign_msg.encode()).digest()
     if isinstance(pub_key, str):
         pub_key = rsa.PublicKey.load_pkcs1_openssl_pem(pub_key.encode())
     try:
-        rsa.verify(hashed_origin, sign_, pub_key)
+        rsa.verify(sign_msg.encode(), sign_, pub_key)
     except rsa.VerificationError:
         return False
     else:
