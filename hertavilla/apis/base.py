415b3ef3702d21f273da88a9b1067f8478e38e29
MingxuanGame <MingxuanGame@outlook.com>
2023-07-14 18:36:46 +0800

    :sparkles: secret 密文传输支持 (#18)

--- hertavilla/apis/base.py
+++ hertavilla/apis/base.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import hashlib
+import hmac
 import logging
 from typing import Any, Literal
+import warnings
 
-from hertavilla.exception import HTTPStatusError, raise_exception
+from hertavilla.exception import (
+    HTTPStatusError,
+    PubkeyNoneWarning,
+    raise_exception,
+)
 
 from aiohttp import ClientSession
 
@@ -12,18 +19,39 @@ BASE_API = "https://bbs-api.miyoushe.com/vila/api/bot/platform"
 
 
 class _BaseAPIMixin:
-    def __init__(self, bot_id: str, secret: str):
+    def __init__(
+        self,
+        bot_id: str,
+        secret: str,
+        pub_key: str | None = None,
+    ):
         self.bot_id = bot_id
         self.secret = secret
+        self.pub_key = pub_key
+        if pub_key is None:
+            warnings.warn(
+                "Pubkey is None. You're using unencrypted secret. "
+                "Unencrypted secret will be deprecated after August 8, 2023",
+                stacklevel=3,
+                category=PubkeyNoneWarning,
+            )
 
     def _make_header(self, villa_id: int) -> dict[str, str]:
+        if self.pub_key is not None:
+            secret = hmac.new(
+                self.pub_key.encode(),
+                self.secret.encode(),
+                hashlib.sha256,
+            ).hexdigest()
+        else:
+            secret = self.secret
         return {
             "x-rpc-bot_id": self.bot_id,
-            "x-rpc-bot_secret": self.secret,
+            "x-rpc-bot_secret": secret,
             "x-rpc-bot_villa_id": str(villa_id),
         }
 
-    async def base_request(  # noqa: PLR0913
+    async def base_request(
         self,
         api: str,
         method: Literal["POST"] | Literal["GET"],
