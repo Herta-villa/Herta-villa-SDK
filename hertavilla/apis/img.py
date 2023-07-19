82830c4030ee06144f097298b217c8c3bce90744
MingxuanGame <MingxuanGame@outlook.com>
2023-07-19 19:42:25 +0800

    :bug: 修复转存图片鉴权失败错误 (#25)

--- hertavilla/apis/img.py
+++ hertavilla/apis/img.py
@@ -6,11 +6,13 @@ from hertavilla.apis.base import _BaseAPIMixin
 class ImgAPIMixin(_BaseAPIMixin):
     async def transfer_image(
         self,
+        villa_id: int,
         url: str,
     ) -> str:
         """将非米游社的三方图床图片转存到米游社官方图床
 
         Args:
+            villa_id (int): 大别野 id
             url (str): 三方图床的图片链接
 
         Returns:
@@ -20,6 +22,7 @@ class ImgAPIMixin(_BaseAPIMixin):
             await self.base_request(
                 "/transferImage",
                 "POST",
+                villa_id,
                 data={"url": url},
             )
         )["new_url"]
