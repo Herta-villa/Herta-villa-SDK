78de61057af40ac644daefb720058c60a919e68a
MingxuanGame <MingxuanGame@outlook.com>
2023-11-12 23:16:42 +0800

    :feature: support upload images to aliyun OSS (fuck aliyun)

--- hertavilla/apis/img.py
+++ hertavilla/apis/img.py
@@ -1,6 +1,16 @@
 from __future__ import annotations
 
+from hashlib import md5
+from io import BytesIO
+from pathlib import Path
+from typing import Literal
+
 from hertavilla.apis.internal import _BaseAPIMixin
+from hertavilla.exception import HTTPStatusError
+from hertavilla.model import UploadParams
+from hertavilla.utils import CustomFormData
+
+from aiohttp import ClientSession
 
 
 class ImgAPIMixin(_BaseAPIMixin):
@@ -26,3 +36,100 @@ class ImgAPIMixin(_BaseAPIMixin):
                 data={"url": url},
             )
         )["new_url"]
+
+    async def get_upload_image_params(
+        self,
+        villa_id: int,
+        md5: str,
+        ext: Literal["jpg", "png", "jpeg", "bmp", "gif"],
+    ) -> UploadParams:
+        """获取米游社阿里云 OSS 上传参数
+
+        Args:
+            villa_id (int): 大别野 id
+            md5 (str): 图片的 md5，最后会被用于文件名
+            ext (Literal[&quot;jpg&quot;, &quot;png&quot;, &quot;jpeg&quot;, &quot;bmp&quot;, &quot;gif&quot;]): 图片扩展名 (支持 jpg,jpeg,png,gif,bmp)
+
+        Returns:
+            UploadParams: 上传参数
+        """  # noqa: E501
+        return await self.base_request(
+            "/getUploadImageParams",
+            "GET",
+            villa_id,
+            params={"md5": md5, "ext": ext},
+        )
+
+    async def upload_to_aliyun_oss(
+        self,
+        image: bytes,
+        params: UploadParams,
+    ) -> str:
+        """上传图片到阿里云 OSS
+
+        Args:
+            image (bytes): 图片二进制
+            params (UploadParams): 上传参数
+
+        Returns:
+            str: 图片 URL
+        """
+        form = CustomFormData(
+            {
+                "x:extra": params["params"]["callback_var"]["x:extra"],
+                "OSSAccessKeyId": params["params"]["accessid"],
+                "signature": params["params"]["signature"],
+                "success_action_status": params["params"][
+                    "success_action_status"
+                ],
+                "name": params["params"]["name"],
+                "callback": params["params"]["callback"],
+                "x-oss-content-type": params["params"]["x_oss_content_type"],
+                "key": params["params"]["key"],
+                "policy": params["params"]["policy"],
+            },
+        )
+        # for k, v in .items():
+        #     form.add_field(k, v, content_type="text/plain")
+        form.add_field(
+            "file",
+            image,
+            filename=params["file_name"],
+            content_type="image/png",
+        )
+        async with ClientSession() as session:
+            async with session.post(
+                params["params"]["host"],
+                data=form,
+            ) as resp:
+                await resp.json()
+                if not resp.ok:
+                    raise HTTPStatusError(resp.status)
+                return (await resp.json())["data"]["url"]
+
+    async def upload_image(
+        self,
+        villa_id: int,
+        image: bytes | BytesIO | Path,
+        ext: Literal["jpg", "png", "jpeg", "bmp", "gif"],
+    ) -> str:
+        """上传图片（快捷方法）
+
+        Args:
+            villa_id (int): 大别野 id
+            image (bytes): 图片
+            ext (Literal[&quot;jpg&quot;, &quot;png&quot;, &quot;jpeg&quot;, &quot;bmp&quot;, &quot;gif&quot;]): 图片扩展名
+
+        Returns:
+            str: 图片 URL
+        """  # noqa: E501
+        if isinstance(image, BytesIO):
+            image = image.getvalue()
+        elif isinstance(image, Path):
+            image = image.read_bytes()
+        params = await self.get_upload_image_params(
+            villa_id,
+            md5(image).hexdigest(),
+            ext,
+        )
+        return await self.upload_to_aliyun_oss(image, params)
--- hertavilla/utils.py
+++ hertavilla/utils.py
@@ -2,6 +2,15 @@ from __future__ import annotations
 
 import json
 
+from aiohttp import (
+    BytesPayload,
+    FormData,
+    MultipartWriter,
+    Payload,
+    StringPayload,
+    hdrs,
+)
+
 
 class MsgEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -22,3 +31,82 @@ def _c(text: str) -> int:
 
 def _rc(length: int) -> int:
     return (length + 1) * 2
+
+
+class CustomPayload(Payload):
+    @property
+    def _binary_headers(self) -> bytes:
+        # fuck aliyun
+        headers = self.headers.copy()
+        disposition = (
+            "Content-Disposition: "
+            f"{headers.pop('Content-Disposition')}\r\n".encode()
+        )
+        return disposition + (
+            (
+                "".join([f"{k}: {v}\r\n" for k, v in headers.items()]).encode(
+                    "utf-8",
+                )
+                + b"\r\n"
+            )
+        )
+
+
+class CustomBytesPayload(BytesPayload, CustomPayload):
+    pass
+
+
+class CustomStringPayload(StringPayload, CustomPayload):
+    pass
+
+
+def make_payload(value, **kwargs) -> CustomBytesPayload | CustomStringPayload:
+    return (
+        CustomBytesPayload(value, **kwargs)
+        if isinstance(value, (bytes, bytearray, memoryview))
+        else CustomStringPayload(value, **kwargs)
+    )
+
+
+class CustomFormData(FormData):
+    def _gen_form_data(self) -> MultipartWriter:
+        # the majority of this is copy pasted from aiohttp
+        """Encode a list of fields using the multipart/form-data MIME format"""
+        if self._is_processed:
+            raise RuntimeError("Form data has been processed already")
+        for dispparams, headers, value in self._fields:
+            try:
+                if hdrs.CONTENT_TYPE in headers:
+                    part = make_payload(
+                        value,
+                        content_type=headers[hdrs.CONTENT_TYPE],
+                        headers=headers,
+                        encoding=self._charset,
+                    )
+                else:
+                    part = make_payload(
+                        value,
+                        headers=headers,
+                        encoding=self._charset,
+                    )
+            except Exception as exc:
+                raise TypeError(
+                    "Can not serialize value type: %r\n "
+                    "headers: %r\n value: %r" % (type(value), headers, value),
+                ) from exc
+
+            if dispparams:
+                part.set_content_disposition(
+                    "form-data",
+                    quote_fields=self._quote_fields,
+                    **dispparams,
+                )
+                # FIXME cgi.FieldStorage doesn't likes body parts with
+                # Content-Length which were sent via chunked transfer encoding
+                assert part.headers is not None
+                part.headers.popall(hdrs.CONTENT_LENGTH, None)
+
+            self._writer.append_payload(part)
+
+        self._is_processed = True
+        return self._writer
